c4adefd44c7e1ccb1cb551c9e47e30b74058a98d
Olukayode <olukayodeshokunbi@gmail.com>
2021-08-27 00:55:06 -0400

    Add condition to check query string(s) sent in request url, and pass  it as a filter to DB collection query

--- routes/sale.js
+++ routes/sale.js
@@ -4,9 +4,57 @@ const Sale = require("../models/Sale");
 
 // get sales to a sppecific user
 router.get("/", async (req, res) => {
-  const query = req.query;
+  const {
+    saleDate,
+    storeLocation,
+    couponUsed,
+    purchaseMethod,
+    gender,
+    age,
+    email,
+    satisfaction,
+  } = req.query;
 
-  const sale = await Sale.find({ "customer.email": query.email });
+  const queryObject = {};
+
+  if (gender) {
+    queryObject["customer.gender"] = gender;
+  }
+
+  if (age) {
+    queryObject["customer.age"] = +age;
+  }
+
+  if (email) {
+    queryObject["customer.email"] = email;
+  }
+  if (satisfaction) {
+    queryObject["customer.satisfaction"] = satisfaction;
+  }
+
+  if (saleDate) {
+    queryObject.saleDate = saleDate;
+  }
+
+  if (storeLocation) {
+    queryObject.storeLocation = storeLocation;
+  }
+
+  if (couponUsed) {
+    queryObject.couponUsed = couponUsed;
+  }
+
+  if (purchaseMethod) {
+    queryObject.purchaseMethod = purchaseMethod;
+  }
+
+  console.log(queryObject);
+
+  let selectOptions = Object.keys(queryObject);
+  selectOptions = selectOptions.join(" ");
+  console.log(selectOptions);
+
+  const sale = await Sale.find(queryObject).limit(2).select(selectOptions);
   res.status(200).json({ sale, count: sale.length });
 });
 
